dc801e54add1c95d5c6e0c77356d1bc6ee288513
fenggp <18319407429@163.com>
2024-04-12 02:05:24 +0800

    order api

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { eq, sql, sum } from 'drizzle-orm';
 import { DbService } from 'src/db/db.service';
-import { orderDetails, orderView } from 'src/mysql/schema';
+import { orderDetails, orders } from 'src/mysql/schema';
 
 @Injectable()
 export class OrderService {
   constructor(private readonly dbService: DbService) {}
 
   async orderLists() {
-    const list = await this.dbService.mysql.select().from(orderView);
+    const list = await this.dbService.mysql
+      .select()
+      .from(orders)
+      .leftJoin(orderDetails, eq(orders.id, orderDetails.orderId));
     return list;
   }
 }
